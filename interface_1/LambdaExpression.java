09cbb6694605210224d05037491765baae199df4
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-01 12:18:52 +0530

    changes

---
+++ interface_1/LambdaExpression.java
@@ -0,0 +1,18 @@
+package interface_1;
+@FunctionalInterface
+interface A {
+    int add( int a ,int b);
+}
+public class LambdaExpression {
+    public static void main(String[] args) {
+    //    A obj = (int a, int b)->{
+    //          return a+b;
+    //       };
+      A obj = (a,b)->a+b;// beauty of lambda expression `
+       int result = obj.add(4, 7);
+       System.out.println(result);
+            
+        
+       
+    }
+}
