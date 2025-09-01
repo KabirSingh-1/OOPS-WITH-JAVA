96a59c88499cd373d0241ddbdb812fa3208a7219
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-01 12:18:33 +0530

    changes

---
+++ Inner_AnonymousClasses/AnonymousClass/AnonumousInnerClass.java
@@ -0,0 +1,17 @@
+package Inner_AnonymousClasses.AnonymousClass;
+
+class A{
+    public void show() {
+        System.out.println("in show");
+    }
+}
+public class AnonumousInnerClass {
+    public static void main(String[] args) {
+        A a = new A() {
+            public void show() {
+                System.out.println("in anonymous class");
+            }
+        };
+        a.show();
+    }
+}
